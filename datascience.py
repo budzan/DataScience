03582ff9b4c881dc088277dc02c2e2640e9f0133
Klaudia <klaudia.budzanowska@gmail.com>
2024-10-31 16:28:43 +0100

    Initial commit

---
+++ datascience.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Import dataset
+df = pd.read_csv("C:/Resources/Python/Data Science/Telco-customer-churn.csv")
+print(df.head())
+
+# Data Manipulation
